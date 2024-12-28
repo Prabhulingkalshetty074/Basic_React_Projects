f1242ca31ad0a380e9d7c3c7e721cbf5d8aeea0d
Prabhulingkalshetty074 <prabhuling074@gmail.com>
2024-12-28 15:08:33 +0530

    props

---
+++ basics/src/Greeting.jsx
@@ -0,0 +1,5 @@
+export default function Greeting(props){
+    return(
+        <div><h3>hello{props.name}</h3></div>
+    )
+}
---
+++ basics/src/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return(
+        <>
+        <h1>Some Heading</h1>
+        <header>Hedare Content</header>
+        </>
+    )
+}
